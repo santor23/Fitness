ce8cc48dbc5c485381d068d5a9685e51b22f7707
santor23 <babloo.singh25@gmail.com>
2014-04-15 07:34:16 +0100

    add Interface EmpdatabainterfaCE

---
+++ employee/EmpCmpTable.java
@@ -0,0 +1,19 @@
+package com.example.contentprovoderdetails;
+
+public interface EmpCmpTable 
+{
+	public static final String EMP_TABLE ="Employee";
+	
+	public static final String EMP_NAME ="empname";
+	public static final String EMP_ADDRESS ="address";
+	public static final String   EMP_SALARY ="salary";
+	public static final String EMP_COMPANYNAME ="compname";
+	
+	public static final String EMP_ID="empid";
+	
+	
+	
+	
+	
+	
+}
